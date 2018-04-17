e49c53f01f290fc9d15608d0f41764efbfcf72a7
varsin <varsinnn@gmail.com>
2018-04-17 01:32:56 +0000

    Supress the unwanted output

--- add-new-local-user.sh
+++ add-new-local-user.sh
@@ -12,14 +12,14 @@
 
 if [[ "${UID}" -ne 0 ]]
 then
-	echo "Please run with sudo or as root"
+	echo "Please run with sudo or as root" >&2
 	exit 1
 fi
 
 if [[ "${#}" -lt 1 ]]
 then
-	echo "Usage: ${0} USER_NAME [COMMENT]..."
-	echo "Create the account with USER_NAME and a comments field on COMMENT"
+	echo "Usage: ${0} USER_NAME [COMMENT]..." >&2
+	echo "Create the account with USER_NAME and a comments field on COMMENT" >&2
 	exit 1
 fi
 
@@ -47,27 +47,25 @@ fi
 
 #Create the user with the password
 
-useradd -c "${COMMENT}" -m  ${USER_NAME}
+useradd -c "${COMMENT}" -m  ${USER_NAME} &> /dev/null
 
 if [[ "${?}" -ne 0  ]]
 then
-	echo "The account could not be created."
+	echo "The account could not be created." >&2
 fi 
 
 #Set the password 
-echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+echo ${PASSWORD} | passwd --stdin ${USER_NAME} &> /dev/null
 
 if [[ "${?}" -ne 0  ]]
 then
-	echo "Password for the account couldn't generated!"
+	echo "Password for the account couldn't generated!" >&2
 	exit 1
 fi 
 
 #Force to change on first login
 
-passwd -e ${USER_NAME}
-
-echo
+passwd -e ${USER_NAME} &> /dev/null
 echo "username:"
 echo "${USER_NAME}"
 echo 
